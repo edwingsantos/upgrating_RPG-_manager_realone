7ab1f64d604d74f5ba0b4c54e7297902c4920933
edwingsantos <edwing.santos@ucas-edu.net>
2026-04-03 09:52:17 -0600

    trying to figure out pandas

--- main.py
+++ main.py
@@ -1,7 +1,54 @@
-#ES main file 
+##ES main file 
+#
+##make a funtio for main 
+#from faker import Faker
+#fake = Faker()
+#print(fake.profile())
+#
+#from faker import Faker 
+#
+#fake = Faker('EN_US')      
+#
+#for i in range(0,2): 
+#    print('Name->', fake.name(),"\n",'Country->', fake.country())
+#
+#import matplotlib.pyplot as plt
+#import numpy as np
+#
+#xpoints = np.array([1, 8])
+#ypoints = np.array([3, 10])
+#
+#plt.plot(xpoints, ypoints)
+#plt.show()
+#
+#import pandas as pd
+#from faker import Faker
+#
+#fake = Faker()
+#
+## Generate a list of 100 fake user records
+#data = [
+#    {
+#        "Name": fake.name_female(),
+#        "Email": fake.email(),
+#        "Address": fake.address(),
+#        "Join Date": fake.date_this_decade()
+#    }
+#    for _ in range(3)
+#]
 
-#make a funtio for main 
+#df = pd.DataFrame(data)
+ 
+# Preview the results
+#print(df.head())
 
-from faker import Faker
-fake = Faker()
-print(fake.profile())
+import pandas as pd
+
+pd.index = pd.index + 1
+
+
+calories = [{"day1": 420, "day2": 380, "day3": 390}]
+
+myvar = pd.DataFrame(calories)
+
+print(myvar)
